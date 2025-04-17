cc2f09dea6eb3b21f2d41de7a5be2323a66f0782
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-17 22:40:13 +0530

    Add HeroSection component to RootPage

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@
 
 import NavigationBar from "@/components/NavigationBar";
 import React from "react";
-
+import HeroSection from "./components/HeroSection";
 
 const RootPage = () => {
   return (
     <div className="flex flex-col items-stretch">
       <NavigationBar />
-      
+      <HeroSection />
     </div>
   );
 };
